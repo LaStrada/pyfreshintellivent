24e9cc587208dccc2124aa0a2f103a3bdf6598ae
Ståle Semb Hauknes <stale.hauknes@me.com>
2021-09-01 22:29:45 +0200

    Fix a Helpers.py import bug (#7)

--- pyfreshintellivent/helpers.py
+++ pyfreshintellivent/helpers.py
@@ -1,6 +1,6 @@
 import binascii
 
-def validatedRPM(self, value):
+def validatedRPM(value):
     if value < 800:
         return 800
     elif value > 2400:
@@ -8,7 +8,7 @@ def validatedRPM(self, value):
     else:
         return value
 
-def validatedDetection(self, value):
+def validatedDetection(value):
     if value < 0:
         return 0
     elif value > 3:
@@ -16,14 +16,14 @@ def validatedDetection(self, value):
     else:
         return int(value)
 
-def validatedMinutes(self, value):
+def validatedMinutes(value):
     if value < 0:
         return 0
     else:
         return int(value)
 
-def bytesToString(self, value):
+def bytesToString(value):
     return
 
-def stringToBytes(self, value):
+def stringToBytes(value):
     return binascii.b2a_hex(value).decode('utf-8')
