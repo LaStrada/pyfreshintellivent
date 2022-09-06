ac65462edcce1acca13beddad656ea81632e1858
Ståle Storø Hauknes <stale.hauknes@gmail.com>
2022-09-07 00:57:54 +0200

    Lint fixes

--- pyfreshintellivent/FreshIntellivent.py
+++ pyfreshintellivent/FreshIntellivent.py
@@ -5,11 +5,7 @@ from struct import pack, unpack
 
 
 class Sky(object):
-
-    def __init__(self,
-                 addr=None,
-                 auth=None,
-                 debug=False):
+    def __init__(self, addr=None, auth=None, debug=False):
         self._auth = False
         self._debug = debug
         self._conn = ble.Peripheral()
@@ -43,9 +39,9 @@ class Sky(object):
         self._log("Info", "Authenticating...")
 
         try:
-            self._conn.getCharacteristics(
-                uuid=characteristics.AUTH)[0].write(
-                    bytes.fromhex(auth), withResponse=True)
+            self._conn.getCharacteristics(uuid=characteristics.AUTH)[0].write(
+                bytes.fromhex(auth), withResponse=True
+            )
 
             self._log("Info", "Authenticated!")
 
@@ -68,8 +64,7 @@ class Sky(object):
     def _writeCharacteristic(self, uuid, value):
         self._log("W", "{} = {}".format(uuid, value))
         try:
-            self._conn.getCharacteristics(uuid=uuid)[0].write(
-                value, withResponse=True)
+            self._conn.getCharacteristics(uuid=uuid)[0].write(value, withResponse=True)
 
         except Exception as e:
             self._log("Error", "Write error - {}".format(str(e)))
@@ -77,13 +72,10 @@ class Sky(object):
 
     def _log(self, level, message):
         if self._debug:
-            print("[Fresh Intellivent Sky] [{}] {}").format(
-                level, message)
+            print("[Fresh Intellivent Sky] [{}] {}").format(level, message)
 
     def getHumidity(self):
-        value = unpack(
-            '<BBH',
-            self._readCharacterisitc(uuid=characteristics.HUMIDITY))
+        value = unpack("<BBH", self._readCharacterisitc(uuid=characteristics.HUMIDITY))
 
         self.humidityEnabled = bool(value[0])
         self.humidityDetection = value[1]
@@ -92,22 +84,23 @@ class Sky(object):
         return {
             "enabled": self.humidityEnabled,
             "detection": self.humidityDetection,
-            "rpm": self.humidityRPM
+            "rpm": self.humidityRPM,
         }
 
     def setHumidity(self, humidityEnabled, humidityDetection, humidityRPM):
-        value = pack(
-            '<BBH',
-            humidityEnabled,
-            h.validatedDetection(humidityDetection),
-            h.validatedRPM(humidityRPM))
+        value = (
+            pack(
+                "<BBH",
+                humidityEnabled,
+                h.validatedDetection(humidityDetection),
+                h.validatedRPM(humidityRPM),
+            ),
+        )
 
         self._writeCharacteristic(characteristics.HUMIDITY, value)
 
     def getLightVOC(self):
-        value = unpack(
-            '<4B',
-            self._readCharacterisitc(uuid=characteristics.LIGHT_VOC))
+        value = unpack("<4B", self._readCharacterisitc(uuid=characteristics.LIGHT_VOC))
 
         lightEnabled = bool(value[0])
         lightDetection = value[1]
@@ -115,55 +108,38 @@ class Sky(object):
         vocDetection = value[3]
 
         return {
-            "light": {
-                "enabled": lightEnabled,
-                "detection": lightDetection
-            },
-            "voc": {
-                "enabled": vocEnabled,
-                "detection": vocDetection
-            }
+            "light": {"enabled": lightEnabled, "detection": lightDetection},
+            "voc": {"enabled": vocEnabled, "detection": vocDetection},
         }
 
-    def setLightVOC(self,
-                    lightEnabled,
-                    lightDetection,
-                    vocEnabled,
-                    vocDetection):
+    def setLightVOC(self, lightEnabled, lightDetection, vocEnabled, vocDetection):
         value = pack(
-            '<4b',
+            "<4b",
             bool(lightEnabled),
             h.validatedDetection(lightDetection),
             bool(vocEnabled),
-            h.validatedDetection(vocDetection))
+            h.validatedDetection(vocDetection),
+        )
 
         self._writeCharacteristic(characteristics.LIGHT_VOC, value)
 
     def getConstantSpeed(self):
         value = unpack(
-            '<BH',
-            self._readCharacterisitc(uuid=characteristics.CONSTANT_SPEED))
+            "<BH", self._readCharacterisitc(uuid=characteristics.CONSTANT_SPEED)
+        )
 
         constantSpeedEnabled = bool(value[0])
         constantSpeedRPM = value[1]
 
-        return {
-            "enabled": constantSpeedEnabled,
-            "rpm": constantSpeedRPM
-        }
+        return {"enabled": constantSpeedEnabled, "rpm": constantSpeedRPM}
 
     def setConstantSpeed(self, constantSpeedEnabled, constantSpeedRPM):
-        value = pack(
-            '<BH',
-            constantSpeedEnabled,
-            h.validatedRPM(constantSpeedRPM))
+        value = pack("<BH", constantSpeedEnabled, h.validatedRPM(constantSpeedRPM))
 
         self._writeCharacteristic(characteristics.CONSTANT_SPEED, value)
 
     def getTimer(self):
-        value = unpack(
-            '<3BH',
-            self._readCharacterisitc(uuid=characteristics.TIMER))
+        value = unpack("<3BH", self._readCharacterisitc(uuid=characteristics.TIMER))
 
         timerRunningTime = value[0]
         timerDelayEnabled = bool(value[1])
@@ -171,110 +147,85 @@ class Sky(object):
         timerRPM = value[3]
 
         return {
-            "delay": {
-                "enabled": timerDelayEnabled,
-                "minutes": timerDelayMinutes
-            },
+            "delay": {"enabled": timerDelayEnabled, "minutes": timerDelayMinutes},
             "runTime": timerRunningTime,
-            "rpm": timerRPM
+            "rpm": timerRPM,
         }
 
-    def setTimer(self,
-                 timerRunningTime,
-                 timerDelayEnabled,
-                 timerDelayMinutes,
-                 timerRPM):
+    def setTimer(
+        self, timerRunningTime, timerDelayEnabled, timerDelayMinutes, timerRPM
+    ):
         value = pack(
-            '<3BH',
+            "<3BH",
             timerRunningTime,
             bool(timerDelayEnabled),
             timerDelayMinutes,
-            h.validatedRPM(timerRPM))
+            h.validatedRPM(timerRPM),
+        )
 
         self._writeCharacteristic(characteristics.TIMER, value)
 
     def getAiring(self):
-        value = unpack(
-            '<3BH',
-            self._readCharacterisitc(uuid=characteristics.AIRING))
+        value = unpack("<3BH", self._readCharacterisitc(uuid=characteristics.AIRING))
 
         airingEnabled = bool(value[0])
         airingRunTime = value[2]
         airingRPM = value[3]
 
-        return {
-            "enabled": airingEnabled,
-            "runTime": airingRunTime,
-            "rpm": airingRPM
-        }
+        return {"enabled": airingEnabled, "runTime": airingRunTime, "rpm": airingRPM}
 
     def setAiring(self, airingEnabled, airingRunTime, airingRPM):
         value = pack(
-            '<3BH',
+            "<3BH",
             bool(airingEnabled),
             int(26),  # Always 1A? (= 26)
             h.validatedMinutes(airingRunTime),
-            h.validatedRPM(airingRPM))
+            h.validatedRPM(airingRPM),
+        )
 
         self._writeCharacteristic(characteristics.AIRING, value)
 
     def getPause(self):
-        value = unpack(
-            '<2B',
-            self._readCharacterisitc(uuid=characteristics.PAUSE))
+        value = unpack("<2B", self._readCharacterisitc(uuid=characteristics.PAUSE))
 
         pauseEnabled = bool(value[0])
         pauseMinutes = value[1]
 
-        return {
-            "enabled": pauseEnabled,
-            "minutes": pauseMinutes
-        }
+        return {"enabled": pauseEnabled, "minutes": pauseMinutes}
 
     def setPause(self, pauseEnabled, pauseMinutes):
-        value = pack(
-            '<2B',
-            bool(pauseEnabled),
-            h.validatedMinutes(pauseMinutes))
+        value = pack("<2B", bool(pauseEnabled), h.validatedMinutes(pauseMinutes))
 
         self._writeCharacteristic(characteristics.PAUSE, value)
 
     def getBoost(self):
-        value = unpack(
-            '<B2H',
-            self._readCharacterisitc(uuid=characteristics.BOOST))
+        value = unpack("<B2H", self._readCharacterisitc(uuid=characteristics.BOOST))
 
         boostEnabled = bool(value[0])
         boostMinutes = value[1]
         boostRPM = value[2]
 
-        return {
-            "enabled": boostEnabled,
-            "minutes": boostMinutes,
-            "rpm": boostRPM
-        }
+        return {"enabled": boostEnabled, "minutes": boostMinutes, "rpm": boostRPM}
 
     def setBoost(self, boostEnabled, boostMinutes, boostRPM):
         value = pack(
-            '<2B',
+            "<2B",
             bool(boostEnabled),
             h.validatedMinutes(boostMinutes),
-            h.validatedRPM(boostRPM))
+            h.validatedRPM(boostRPM),
+        )
 
         self._writeCharacteristic(characteristics.BOOST, value)
 
     def setTemporarySpeed(self, temporaryEnabled, temporaryRPM):
-        value = pack(
-            '<BH',
-            bool(temporaryEnabled),
-            h.validatedRPM(temporaryRPM))
+        value = pack("<BH", bool(temporaryEnabled), h.validatedRPM(temporaryRPM))
 
         self._writeCharacteristic(characteristics.TEMPORARY_SPEED, value)
 
     def getSensorData(self):
         value = unpack(
-            '<2B5HBH',
-            self._readCharacterisitc(uuid=characteristics.DEVICE_STATUS))
+            "<2B5HBH", self._readCharacterisitc(uuid=characteristics.DEVICE_STATUS)
+        )
 
         status = bool(value[0])
         mode = value[1]
@@ -302,11 +253,8 @@ class Sky(object):
 
         return {
             "status": status,
-            "mode": {
-                "value": mode,
-                "description": modeDescription
-            },
+            "mode": {"value": mode, "description": modeDescription},
             "rpm": rpm,
             "secs": secs,
-            "temp": temp
+            "temp": temp,
         }
