ac937a6e75166fe48ba1b3c35460be22f6d6f8d2
Anders Y <anders.yderborg@gmail.com>
2022-11-09 21:24:06 +0100

    Update examples/sky-set-boost-off.py
    Co-authored-by: Ståle Storø Hauknes <stale.hauknes@me.com>

--- examples/sky-set-boost-off.py
+++ examples/sky-set-boost-off.py
@@ -1,3 +1,3 @@
 from pyfreshintellivent.pyfreshintellivent.FreshIntellivent import Sky
-p = Sky("Fan adress","authcode from sky-auth-ex",False)
+p = Sky("Fan address","authcode from sky-auth-ex", False)
 p.setBoost(False,600,2400)
