2359c5e17470ab144e67cfb2aa50c831b42c9722
Ståle Storø Hauknes <stale.semb@airthings.com>
2023-01-18 02:09:59 +0100

    Fix read.py example

--- examples/read.py
+++ examples/read.py
@@ -1,43 +1,58 @@
 import asyncio
+import logging
 import sys
 
 from bleak.backends.device import BLEDevice
 
-from pyfreshintellivent import FreshIntelliVent
+from pyfreshintellivent import FreshIntelliVent, scanner
 
 
 async def main():
-    sky = FreshIntelliVent()
+    client = FreshIntelliVent()
     address = sys.argv[1]
     authentication_code = sys.argv[2]
 
-    ble_device = BLEDevice(address=address)
+    ble_device = await scanner.scan()
+
+    if ble_device is None:
+        print("Couldn't find any devices")
+        return
 
     try:
-        async with sky.connect(ble_device) as client:
+        print("Connecting...")
+        await client.connect(ble_device)
+        print("Connected")
+
+        await client.fetch_device_information()
+
+        if authentication_code is None:
+            print("No authentication code, skipping authentication")
+        else:
             await client.authenticate(authentication_code=authentication_code)
+            print("Authenticated")
 
-            sensors = await client.fetch_sensor_data()
-            print(f"Status: {sensors.as_dict()}")
+        sensors = await client.fetch_sensor_data()
+        print(f"Status: {sensors.as_dict()}")
 
-            boost = await client.fetch_boost()
-            print(f"Boost: {boost}")
+        boost = await client.fetch_boost()
+        print(f"Boost: {boost}")
 
-            constant_speed = await client.fetch_constant_speed()
-            print(f"Constant speed: {constant_speed}")
+        constant_speed = await client.fetch_constant_speed()
+        print(f"Constant speed: {constant_speed}")
 
-            humidity = await client.fetch_humidity()
-            print(f"Humidity: {humidity}")
+        humidity = await client.fetch_humidity()
+        print(f"Humidity: {humidity}")
 
-            light_and_voc = await client.fetch_light_and_voc()
-            print(f"Light and VOC: {light_and_voc}")
+        light_and_voc = await client.fetch_light_and_voc()
+        print(f"Light and VOC: {light_and_voc}")
 
-            pause = await client.fetch_pause()
-            print(f"Pause: {pause}")
+        pause = await client.fetch_pause()
+        print(f"Pause: {pause}")
 
-            timer = await client.fetch_timer()
-            print(f"Timer: {timer}")
+        timer = await client.fetch_timer()
+        print(f"Timer: {timer}")
     except Exception as e:
+        await client.disconnect()
         print(e)
 
 
