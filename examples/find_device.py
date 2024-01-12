7b546e112eec5b13bdc151cd3da26110fce8dab0
Ståle Storø Hauknes <stale.semb@airthings.com>
2024-01-12 14:19:20 +0100

    Remove unused code

--- examples/find_device.py
+++ examples/find_device.py
@@ -2,9 +2,6 @@ import asyncio
 
 from pyfreshintellivent import scanner
 
-AUTHENTICATE_UUID = "4cad343a-209a-40b7-b911-4d9b3df569b2"
-SENSOR_UUID = "528b80e8-c47a-4c0a-bdf1-916a7748f412"
-
 
 async def main():
     print("Scanning, please wait...")
